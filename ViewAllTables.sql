42f8e81cdb4f1920d2c85ef7efce3ba49db6d3ec
David-YY-Berger <91850832+David-YY-Berger@users.noreply.github.com>
2023-04-20 16:12:58 +0300

    Add files via upload

---
+++ ViewAllTables.sql
@@ -0,0 +1,13 @@
+
+-- Use the tabs below to see each one!
+
+select * from Writer;
+select * from WrittenBy;
+select * from advertisement;
+select * from article;
+select * from picturead;
+select * from appearson;
+select * from platform;
+select * from newspaper;
+select * from website;
+
