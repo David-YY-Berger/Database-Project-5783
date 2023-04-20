95cfd960a1c402ec9fc2d141542f87fcace90b81
David-YY-Berger <91850832+David-YY-Berger@users.noreply.github.com>
2023-04-20 16:54:07 +0300

    Add files via upload

---
+++ DeleteValuesKeepTables.sql
@@ -0,0 +1,14 @@
+-- order matters!! 
+
+DELETE FROM AppearsOn;
+DELETE FROM WrittenBy;
+
+DELETE FROM PictureAd;
+DELETE FROM Article;
+DELETE FROM Advertisement;
+
+DELETE FROM Website;
+DELETE FROM Newspaper;
+DELETE FROM Platform;
+
+DELETE FROM Writer;
