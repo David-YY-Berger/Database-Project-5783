ddc593e9e7121cc997d2e86b5e5e44f39a17b564
arielzuki <92029043+arielzuki@users.noreply.github.com>
2023-05-23 23:25:56 +0300

    Update Query 4 - BestAds.sql

--- Queries/Query 4 - BestAds.sql
+++ Queries/Query 4 - BestAds.sql
@@ -1,3 +1,3 @@
-select A.ADNAME, A.HOURSTOWRITE
+select A.ADNAME, W.Numyearsofexperience, A.HOURSTOWRITE
 from Writer W natural join Writtenby WB natural join Advertisement A
 order by W.Numyearsofexperience desc, A.HOURSTOWRITE desc
