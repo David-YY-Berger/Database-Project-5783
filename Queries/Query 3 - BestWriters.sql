dbc304e847698646cc04af597137c7b4ecc5ef6e
arielzuki <92029043+arielzuki@users.noreply.github.com>
2023-05-23 23:17:05 +0300

    Update Query 3 - BestWriters.sql

--- Queries/Query 3 - BestWriters.sql
+++ Queries/Query 3 - BestWriters.sql
@@ -1,3 +1,7 @@
-select WRITERNAME, NUMYEARSOFEXPERIENCE, NUMYEARSOFEXPERIENCE/PAYPERHOUR as Raiting
-from Writer
+select distinct WRITERNAME, NUMYEARSOFEXPERIENCE, 
+       cast(NUMYEARSOFEXPERIENCE/PAYPERHOUR*100 as decimal(10,2)) as Raiting,
+       (select count(*)
+       from Writtenby WB
+       where WB.WRITERID = W.WRITERID) as numOfAdds
+from (select * from Writer natural join Writtenby) W
 order by Raiting desc
