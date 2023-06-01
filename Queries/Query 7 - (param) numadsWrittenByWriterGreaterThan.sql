621c415fcce40b706e265e30109e71d24b317015
arielzuki <92029043+arielzuki@users.noreply.github.com>
2023-06-01 18:09:21 +0300

    Update Query 7 - (param) numadsWrittenByWriterGreaterThan.sql

--- Queries/Query 7 - (param) numadsWrittenByWriterGreaterThan.sql
+++ Queries/Query 7 - (param) numadsWrittenByWriterGreaterThan.sql
@@ -1,11 +1,8 @@
 
 select writername, numAds
-from
-
-(select w.writername, w.writerid, count(*) as numAds
-from writer w, writtenBy wb
-where w.writerid = wb.writerid
-group by w.writerid, w.writername
-)
-
-where numAds > 2
+from (select w.writername, w.writerid, count(*) as numAds
+      from writer w, writtenBy wb
+      where w.writerid = wb.writerid
+      group by w.writerid, w.writername
+      )
+where numAds > &numberOfAdds
