78f16339bc2e23b4df77acb4ae7b769248c6ae1b
arielzuki <92029043+arielzuki@users.noreply.github.com>
2023-06-08 16:34:44 +0300

    Delete Query 9 - (param) writerAndAdBetweenDates.sql

--- Queries/Query 9 - (param) writerAndAdBetweenDates.sql
+++
@@ -1,4 +0,0 @@
-select writerid, writername, adid, adname, adtypeappears, platformtype
-from Writer w natural join Writtenby wb natural join Advertisement a natural join Appearson ao
-where ao.datepublished>='&date1'
-order by ao.datepublished
