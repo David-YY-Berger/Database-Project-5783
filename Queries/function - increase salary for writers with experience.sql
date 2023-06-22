88747c2b55aa0680e902720b885f962990305ba3
David Berger <91850832+David-YY-Berger@users.noreply.github.com>
2023-06-22 11:59:36 +0300

    orig

--- Queries/function - increase salary for writers with experience.sql
+++ Queries/function - increase salary for writers with experience.sql
@@ -1,14 +1,15 @@
 CREATE OR REPLACE FUNCTION increase_salary(p_experience NUMBER, p_increase_amount NUMBER) RETURN NUMBER IS
-  v_total_updated NUMBER := 0;
-BEGIN
-  FOR writer_rec IN (
+  CURSOR writer_cursor IS
     SELECT w.writerid, w.payperhour
     FROM writer w
-    WHERE w.numyearsofexperience > p_experience
-  ) LOOP
+    WHERE w.numyearsofexperience > p_experience;
+   
+  v_total_updated NUMBER := 0;
+BEGIN
+  FOR writer_rec IN writer_cursor LOOP
     UPDATE writer w
     SET w.payperhour = writer_rec.payperhour + p_increase_amount
-    WHERE w.writerid = writer_rec.writerid;
+    WHERE id = writer_rec.writerid;
    
     v_total_updated := v_total_updated + 1;
   END LOOP;
@@ -17,7 +18,7 @@ BEGIN
  
   RETURN v_total_updated;
 END;
-/
+
 
 DECLARE
   v_updated_count NUMBER;
@@ -26,4 +27,3 @@ BEGIN
   
   DBMS_OUTPUT.PUT_LINE('Number of writers whose salary was updated: ' || v_updated_count);
 END;
-/
